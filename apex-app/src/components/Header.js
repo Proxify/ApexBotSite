6330dbd4a3a3024619c9081e0f09e03fd0a473aa
Proxify <brad.will24@gmail.com>
2022-12-31 20:33:55 -0600

    Refactoring for React Router

--- apex-app/src/components/Header.js
+++ apex-app/src/components/Header.js
@@ -1,21 +1,23 @@
 import logo from '../resources/apex_logo.svg';
 import '../App.css';
+import { useLocation } from 'react-router';
+import { Outlet, Link, useNavigate } from "react-router-dom";
 
 function Header(props) {
     const bots = props.bots;
-    const selectedBot = props.selectedBot;
+    const location = useLocation();
+    const selectedBot = location.pathname.replace('/bots/', '');
+    const navigate = useNavigate();
 
     const botItems = () => {
         return bots.map(element => {
-            //console.log({selectedBot});
-            //console.log({element});
             const className = selectedBot === element ? "App-header-bot-item-selected" : "App-header-bot-item";
-            return <button className={className} onClick={() => props.clickEvent(element)}>{element}</button>
+            return <button className={className} key={element} onClick={() => navigate(`/bots/${element}`)}>{element}</button>
         });
     }
     return (
         <header className="App-header">
-            <img src={logo} className="App-logo" alt="ApexBots" onClick={() => props.clickEvent("Home")}/>
+            <img src={logo} className="App-logo" alt="ApexBots" onClick={() => navigate('/')}/>
 
             <div className="App-header-separator"></div>
             <div className="App-header-bots">
